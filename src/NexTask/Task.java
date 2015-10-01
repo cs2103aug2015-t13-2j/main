e7ebc01de37b5370ea4d29da3a7019e136682fac
Jingjing Wang <jingwin@outlook.com>
2015-10-01 11:25:41 +0800

    Based on "collate tool", will do changes later

---
+++ src/NexTask/Task.java
@@ -0,0 +1,15 @@
+
+
+
+public class Task {
+	private String name;
+	private String content;
+	
+	public Task(String name){
+		this.name = name;
+	}
+	
+	public String toString(){
+		return content;
+	}
+}
