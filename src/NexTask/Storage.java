0ad6f299a6ea7987d127066e9cc03597a73fa9a1
Jingjing Wang <jingwin@outlook.com>
2015-10-08 20:25:15 +0800

    storage

--- src/NexTask/Storage.java
+++ src/NexTask/Storage.java
@@ -13,27 +13,30 @@ import java.util.ArrayList;
  */
 public class Storage {
     
-    private static final String fileName = "NexText.txt";
+	private static final String DEFAULT_SAVE_DIRECTORY = "NexTask";
+	private static final String SAVE_DIRECTORY_FORMAT = "%s\\NexTask";
+	private static final String FILE_PATH_FORMAT = "%s\\%s.txt";
     private static final String ERROR_FILE_NOT_FOUND = "File was not found: %s";
+    private static final String FILE_NAME = "NexTask";
+    private static int taskNum = 1;
     private ArrayList<Task> tasks;
     private String savePath;
     
     
     public Storage(String directory, ArrayList<Task> tasks) {
-        this.savePath = directory + fileName;
+        this.savePath = String.format(SAVE_DIRECTORY_FORMAT, directory);;
         this.tasks = tasks;
         createSaveDirectory(savePath);      
     }
     
     public Storage() {
-        savePath = System.getProperty("user.dir");
+        savePath = DEFAULT_SAVE_DIRECTORY;
         // This is to get the current directory of the program.
         createSaveDirectory(savePath);
     }
 
     private void createSaveDirectory(String directory) {
         File dir = new File(directory);
-
         if (!dir.exists()) {
             dir.mkdir();
         }
@@ -41,12 +44,14 @@ public class Storage {
 
     public void storeToFile() {
         createSaveDirectory(savePath);
-        try (PrintWriter writer = new PrintWriter(savePath)) {
+        try (PrintWriter writer = new PrintWriter(String.format(FILE_PATH_FORMAT, savePath, FILE_NAME))) {
             for (Task line : tasks) {
-                writer.println(line.toString());
+            		
+                writer.println(taskNum + line.getName());
+                taskNum ++;
             }
           } catch (FileNotFoundException e) {
-              System.out.println(String.format(ERROR_FILE_NOT_FOUND, fileName));
+              System.out.println(String.format(ERROR_FILE_NOT_FOUND, FILE_NAME));
           }
       }
     
