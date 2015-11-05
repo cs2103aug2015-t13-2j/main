713d859f842b3c8e0daaf1488ea6d4ca31fcd367
jennyvien <jennyvien@umail.ucsb.edu>
2015-11-05 12:54:45 +0800

    temp

--- src/test/StorageTest.java
+++ src/test/StorageTest.java
@@ -2,15 +2,12 @@ package test;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 import NexTask.*;
 
 public class StorageTest {
 	private String userPath = "Desktop";
-	private ArrayList<Task> testArray = new ArrayList<Task>();
 
 	@Test
 	public void testGetPath() {
@@ -35,13 +32,4 @@ public class StorageTest {
 		storage.markComplete(0);
 		assertEquals(storage.getCommandSize(), 0);
 	}
-
-	@Test
-	public void testDelete() {
-		Storage storage = Storage.getInstance();
-		Task todo = new Task("a");
-		testArray.add(todo);
-		assertEquals(0, 0);
-	}
-
 }
